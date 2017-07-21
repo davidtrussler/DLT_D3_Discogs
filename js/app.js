d35123df00a552108c779e86fcd8d39ab9d44ac4
David Trussler <mail@davidtrussler.net>
2017-07-21 16:55:38 +0100

    Adds request to get collection data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-document.addEventListener('DOMContentLoaded', init, false);
+/*
+ * app.js
+**/
 
-function init() {
-  console.log('init!')
+var App = function() {};
+
+App.prototype._init = function() {
+  console.log('init!');
+
+  var _this = this;
+  var url = 'https://api.discogs.com/users/davidTrussler/collection/folders/0/releases?per_page=500';
+
+  $.get(url, function(data) {
+    _this._doData(data);
+  });
 }
+
+App.prototype._doData = function(data) {
+  console.log('_doData!');
+  console.log(data);
+}
+
+$(document).ready(function() {
+  var app = new App();
+  app._init();
+});
