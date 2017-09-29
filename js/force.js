9ed3600533ca9e8dbfcc7a86ab2aefec1e1d1f8b
David Trussler <mail@davidtrussler.net>
2017-09-29 17:05:09 +0100

    Comment: Minor progress in learning about force layout

--- js/force.js
+++ js/force.js
@@ -8,30 +8,39 @@ var Force = function() {};
 Force.prototype.draw = function() {
   console.log('draw!');
 
-  var w = 800;
-  var h = 600;
+  // var w = 800;
+  // var h = 600;
+
   var nodes = [
     {num: 100},
     {num: 200},
   ];
-  // var links = [
-  //   {
-  //     source: 0,
-  //     target: 1
-  //   }
-  // ];
-  var svg = document.createElement('svg');
+  var links = [
+    {
+      source: 0,
+      target: 1
+    }
+  ];
+
+  // var svg = document.createElement('svg');
+
   var simulation = d3.forceSimulation(nodes)
+    // .force('x', 100)
+    // .force('y', 100)
     .on('tick', ticked);
 
-  svg.style.width = w + 'px';
-  svg.style.height = h + 'px';
+  console.log('simulation: ', simulation);
 
-  document.getElementById('svg-container').appendChild(svg);
+  // svg.style.width = w + 'px';
+  // svg.style.height = h + 'px';
+
+  // document.getElementById('svg-container').appendChild(svg);
 
   function ticked() {
-    // console.log('ticked!');
+    console.log('ticked!');
+    console.log('nodes: ', nodes);
 
+    /*
     var u = d3.select(svg)
       .selectAll('circle')
       .data(nodes);
@@ -39,14 +48,15 @@ Force.prototype.draw = function() {
     u.enter()
       .append('circle')
       .attr('r', 10)
-      .merge(u)
-      .attr('cx', function(d) {
-        return d.num;
-      })
-      .attr('cy', function(d) {
-        return d.num;
-      });
+      // .attr('cx', function(d) {
+      //   return 100; // d.num;
+      // })
+      // .attr('cy', function(d) {
+      //   return 100; // d.num;
+      // })
+      .merge(u);
 
-    u.exit().remove();
+    // u.exit().remove();
+    */
   }
 }
